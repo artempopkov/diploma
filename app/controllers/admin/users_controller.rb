0a9bdd972115d623aadbe515f0e55eeada2b95c7
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-02 17:13:44 +0300

    Add possibility to edit users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < AdminController
+  before_action :load_models, only: %i[edit update]
+
   def index
     @users = User.order(:id)
   end
@@ -7,12 +9,23 @@ class Admin::UsersController < AdminController
     @user = User.new
   end
 
+  def edit
+  end
+
   def create
     @user = User.new user_params
     if @user.save(validate: false)
       redirect_to admin_users_url, notice: 'Creation finish successfully'
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update user_params
+      redirect_to admin_users_url, notice: "Updating finish successfully"
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
