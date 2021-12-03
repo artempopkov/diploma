a848f162018bb9b9c51848f8084460ba1d10607d
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-03 12:25:47 +0300

    Add possibility to edit moderators

--- app/controllers/admin/moderators_controller.rb
+++ app/controllers/admin/moderators_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ModeratorsController < AdminController
+  before_action :load_models, only: %i[edit update]
 
   def index
     @moderators = Moderator.order(:id)
@@ -8,16 +9,31 @@ class Admin::ModeratorsController < AdminController
     @moderator = Moderator.new
   end
 
+  def edit
+  end
+
   def create
     @moderator = Moderator.new moderator_params
     if @moderator.save(validate: false)
-      redirect_to admin_moderators_url, notice: "Creation finish successfully"
+      redirect_to admin_moderators_url, notice: 'Creation finish successfully'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def update
+    if @moderator.update moderator_params
+      redirect_to admin_moderators_url, notice: 'Updating finish successfully'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def load_models
+    @moderator = Moderator.find params[:id]
+  end
 
   def moderator_params
     params.require(:moderator).permit(:email, :name)
