cfdec323cea4795ae25cfe8f7e4c8636801e6684
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-03 15:02:46 +0300

    Add password field to _form

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,7 +9,7 @@ class Admin::UsersController < AdminController
 
   def create
     @user = User.new user_params
-    if @user.save(validate: false)
+    if @user.save
       redirect_to admin_users_url, notice: 'Creation finish successfully'
     else
       render :new, status: :unprocessable_entity
@@ -23,6 +23,6 @@ class Admin::UsersController < AdminController
   end
 
   def user_params
-    params.require(:user).permit(:email, :name)
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
   end
 end
