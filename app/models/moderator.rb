3b097103c51cd68d274ce96fff2756148ae8789c
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-04 22:20:53 +0300

    Move per_page setting to initializers folder

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -1,4 +1,3 @@
 class Moderator < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
-  self.per_page = 10
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :registerable, :rememberable, :validatable
-  self.per_page = 2
 end
