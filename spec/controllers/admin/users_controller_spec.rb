06b4cc3d975a3cfd4df172d6300246808ac05b61
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-07 12:04:23 +0300

    User controller tests

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -2,9 +2,118 @@ require 'rails_helper'
 
 describe Admin::UsersController, type: :controller do
   describe '#index' do
-    it 'redirects to sign in' do
-      get :index
-      expect(response).to redirect_to(new_moderator_session_path)
+    context 'as an authenticated moderator' do
+      let(:moderator) { FactoryBot.create(:moderator) }
+
+      before do
+        sign_in moderator
+      end
+
+      it 'renders index template' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+
+    context 'as an unauthenticated moderator' do
+      it 'redirects to sign in' do
+        get :index
+        expect(response).to redirect_to(new_moderator_session_path)
+      end
+    end
+  end
+
+  describe '#create' do
+    context 'as an authenticated moderator' do
+      let(:moderator) { FactoryBot.create(:moderator) }
+      let(:user) { FactoryBot.attributes_for(:user) }
+
+      before do
+        sign_in moderator
+      end
+
+      it 'adds a user' do
+        expect do
+          post :create, params: { user: user }
+        end.to change(User, :count).by(1)
+      end
+    end
+
+    context 'as unauthenticated moderator' do
+      let(:user) { FactoryBot.attributes_for(:user) }
+
+      it 'does not add a user' do
+        expect do
+          post :create, params: { user: user }
+        end.to change(User, :count).by(0)
+      end
+
+      it 'redirects to sign in' do
+        post :create, params: { user: user }
+        expect(response).to redirect_to(new_moderator_session_path)
+      end
+    end
+  end
+
+  describe '#update' do
+    context 'as an authenticated moderator' do
+      let(:moderator) { FactoryBot.create(:moderator) }
+      let(:user) { FactoryBot.create(:user) }
+
+      before do
+        sign_in moderator
+      end
+
+      it 'updates a user' do
+        patch :update, params: { id: user.id, user: { name: 'New Name' } }
+        expect(user.reload.name).to eq('New Name')
+      end
+    end
+
+    context 'as an unauthenticated moderator' do
+      let(:user) { FactoryBot.create(:user) }
+
+      it 'does not update a user' do
+        patch :update, params: { id: user.id, user: { name: 'New Name' } }
+        expect(user.reload.name).to_not eq('New Name')
+      end
+
+      it 'redirects to sign in' do
+        patch :update, params: { id: user.id, user: { name: 'New Name' } }
+        expect(response).to redirect_to(new_moderator_session_path)
+      end
+    end
+  end
+
+  describe '#destroy' do
+    context 'as an authenticated moderator' do
+      let(:moderator) { FactoryBot.create(:moderator) }
+      let!(:user) { FactoryBot.create(:user) }
+
+      before do
+        sign_in moderator
+      end
+
+      it 'deletes a user' do
+        expect do
+          delete :destroy, params: { id: user.id }
+        end.to change(User, :count).by(-1)
+      end
+    end
+
+    context 'as an unauthenticated moderator' do
+      let!(:user) { FactoryBot.create(:user) }
+
+      it 'does not delete a user' do
+        expect do
+          delete :destroy, params: { id: user.id }
+        end.to_not change(User, :count)
+      end
+
+      it 'redirects to sign in' do
+        delete :destroy, params: { id: user.id }
+        expect(response).to redirect_to(new_moderator_session_path)
+      end
     end
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
-  factory :moderator do
+  factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 8) }
+    password_confirmation { password }
     name { Faker::Name.name }
   end
 end
