495f4d3e10c00357aa5e9331427c5a04b9c83e70
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-15 11:03:05 +0300

    Add article time creation helper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,9 @@
 module AdminHelper
+  def article_creation_time(article)
+    if article.created_at == article.updated_at
+      article.created_at.strftime('%d %b %Y, %H:%M')
+    else
+      "edit: #{article.updated_at.strftime('%d %b %Y, %H:%M')}"
+    end
+  end
 end
