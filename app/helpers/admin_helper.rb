22da33828c4fb156d628d68e10f28a0813916dd9
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-15 13:08:31 +0300

    Refactor #article_creation_time

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,9 +1,9 @@
 module AdminHelper
   def article_creation_time(article)
-    if article.created_at == article.updated_at
-      article.created_at.strftime('%d %b %Y, %H:%M')
-    else
+    if article.updated?
       "edit: #{article.updated_at.strftime('%d %b %Y, %H:%M')}"
+    else
+      article.created_at.strftime('%d %b %Y, %H:%M')
     end
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
   has_rich_text :content
   has_paper_trail on: [:update]
   validates :title, :description, :content, presence: true
+
+  def updated?
+    Version.where(item_id: id, event: :update).empty? ? false : true
+  end
 end
