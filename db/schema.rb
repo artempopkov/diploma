107340b024b32b12f4bcd99c2b86cb691d1a0fc0
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-20 11:24:42 +0300

    Add status to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_17_131149) do
+ActiveRecord::Schema.define(version: 2021_12_20_082353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_12_17_131149) do
     t.string "avatar"
     t.boolean "avatar_disable"
     t.bigint "category_id"
+    t.integer "status", default: 0, null: false
     t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
