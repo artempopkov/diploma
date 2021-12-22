ea4bbc8bf5942ab07152ca6d953432d036d64f50
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-22 15:03:00 +0300

    Add styled homepage

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -4,3 +4,18 @@ $(window).on('turbolinks:load', function() {
         'overflow': 'visible'
     });
 });
+
+$(document).on('turbolinks:load', function () {
+    $('.search-close').hide();
+    $('button.search-icon').on('click', function () {
+        $(this).hide();
+        $('body').toggleClass("open-search-form");
+        $('.search-close').show();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+    $('.search-close').on('click', function () {
+        $(this).hide();
+        $('body').removeClass("open-search-form");
+        $('button.search-icon').show();
+    });
+});
