48a7dc4266843aaf73430a1fe0a2c285cd00eda5
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-24 16:50:50 +0300

    fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_models, only: %i[show]
+  before_action :load_models, only: %i[show]
 
   def index
     @articles = Article.order(:id)
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
 
   private
 
-  def set_models
+  def load_models
     @article = Article.find(params[:id])
   end
 
