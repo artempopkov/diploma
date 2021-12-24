86150b34481067e7865a7c72c3b07e4bedf40c76
Artem Popkov <artem.popkov@itechart-group.com>
2021-12-24 12:58:12 +0300

    fix

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,9 +1,2 @@
 module AdminHelper
-  def article_creation_time(article)
-    if article.created_at == article.updated_at
-      article.created_at.strftime('%d %b %Y, %H:%M')
-    else
-      "edit: #{article.updated_at.strftime('%d %b %Y, %H:%M')}"
-    end
-  end
 end
