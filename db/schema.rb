4f87ae9802de3b2087c5a86ec852e5e4f777d40b
Artem Popkov <artem.popkov@itechart-group.com>
2022-01-13 15:58:16 +0300

    Add important mark field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_133341) do
+ActiveRecord::Schema.define(version: 2022_01_13_125304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,12 +57,13 @@ ActiveRecord::Schema.define(version: 2022_01_05_133341) do
     t.string "title"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "updated_at"
     t.string "avatar"
     t.boolean "avatar_disable", default: false
     t.bigint "category_id"
     t.integer "status", default: 0, null: false
     t.bigint "moderator_id"
+    t.boolean "important"
     t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["moderator_id"], name: "index_articles_on_moderator_id"
   end
