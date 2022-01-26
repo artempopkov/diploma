fc852f6358b1c9963a15f6843234aa054b82ede5
Artem Popkov <artem.popkov@itechart-group.com>
2022-01-26 16:28:07 +0300

    fix

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -8,11 +8,19 @@ module ArticleHelper
   end
 
   def like_heart(article)
+    return content_tag(:i, '', class: 'icon-heart mr-5') unless current_user
+
     if current_user.liked?(article)
       link_to '', like_article_path(article, 'unlike'), class: 'icon-heart-broken mr-5', method: :put, remote: true
     else
       link_to '', like_article_path(article, 'like'), class: 'icon-heart mr-5', method: :put, remote: true
     end
+  end
+
+  def render_article_preview(article, index)
+    return render 'shared/articles/big_preview_card', article: article if index.zero?
+    
+    render 'shared/articles/medium_preview_card', article: article
   end
 
   def universal_time_format(time)
