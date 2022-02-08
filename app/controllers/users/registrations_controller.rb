a1cb64a11ec15e7ca6284d0927ddf1e60309e2d9
Artem Popkov <artem.popkov@itechart-group.com>
2022-02-08 19:48:14 +0300

    fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
+    yield resource if block_given?
+    if resource_updated
+      set_flash_message_for_update(resource, prev_unconfirmed_email)
+      bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      # respond_with resource, :location => "users/#{resource.id}", template: 'users/show'
+    end
+  end
+
   protected
 
   def after_update_path_for(resource)
