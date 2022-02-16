e152b3781bbffc36d6801381b9eb7d480d20826b
Artem Popkov <artem.popkov@itechart-group.com>
2022-02-16 13:20:11 +0300

    Fix begin rescue

--- app/interactors/articles/prepare.rb
+++ app/interactors/articles/prepare.rb
@@ -3,7 +3,6 @@ module Articles
     include Interactor
 
     def call
-      begin
         context.article.update!(status: 'prepared')
       rescue StandardError => exception
         context.fail!(message: exception.message)
