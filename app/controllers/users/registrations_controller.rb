74e0ca00a4a4ff556c1c7e9e806f5d8cd63fe157
Artem Popkov <artem.popkov@itechart-group.com>
2022-02-18 12:25:03 +0300

    Fix noty

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  layout 'user_profile'
+  layout :resolve_layout
 
   def update
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
@@ -8,13 +8,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
     resource_updated = update_resource(resource, account_update_params)
     yield resource if block_given?
     if resource_updated
+      flash[:notice] = 'Update finish successfully'
       set_flash_message_for_update(resource, prev_unconfirmed_email)
       bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
 
       respond_with resource, location: after_update_path_for(resource)
     else
       clean_up_passwords resource
-      respond_with resource, template: "users/show"
+      respond_with resource
     end
   end
 
@@ -26,5 +27,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def update_resource(resource, params)
     resource.update_without_password(params)
+  end
+
+  private
+
+  def resolve_layout
+    return 'user_profile' if action_name == 'edit' || action_name == 'update'
+
+    'application'
   end
 end
