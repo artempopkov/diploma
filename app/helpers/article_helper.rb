936f7f8612cd8d9e8924bb55fdcd486a59e4f8b3
Artem Popkov <artem.popkov@itechart-group.com>
2022-02-21 19:52:00 +0300

    fix

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,19 +1,19 @@
 module ArticleHelper
   def published_time_label(article)
     utc_published_time = utc(article.published_at)
-    return "Edit: #{utc_published_time}" if article.edited?
+    return 'Edit: #{utc_published_time}' if article.edited?
 
     utc_published_time
   end
 
   def toggle_important(article)
     if article.important?
-      link_to toggle_important_admin_article_path(article, important: false), class: "btn-mark", method: :patch, remote: :true do
-        "Unmark"
+      link_to toggle_important_admin_article_path(article, important: false), class: 'btn-mark', method: :patch, remote: :true do
+        'Unmark'
       end
     else
-      link_to toggle_important_admin_article_path(article, important: true), class: "btn-mark", method: :patch, remote: :true do
-        "Mark"
+      link_to toggle_important_admin_article_path(article, important: true), class: 'btn-mark', method: :patch, remote: :true do
+        'Mark'
       end
     end
   end
@@ -25,7 +25,7 @@ module ArticleHelper
   end
 
   def article_avatar_big_preview_link(article)
-    link_to article, class: "img-link" do
+    link_to article, class: 'img-link' do
       article_avatar_big_preview(article)
     end
   end
@@ -37,24 +37,24 @@ module ArticleHelper
   end
 
   def article_avatar(article)
-    return image_tag(asset_path("no-image.png"), alt: "...") if article.avatar_absent_or_disabled?
+    return image_tag('no-image.png', alt: '...') if article.avatar_absent_or_disabled?
 
-    image_tag(article.avatar.url, alt: "...")
+    image_tag(article.avatar.url, alt: '...')
   end
 
   def article_avatar_big_preview(article)
-    return image_tag(asset_path("no-image.png"), alt: "...") if article.avatar_absent_or_disabled?
+    return image_tag('no-image.png', alt: '...') if article.avatar_absent_or_disabled?
 
-    image_tag(article.avatar.big_preview.url, alt: "...")
+    image_tag(article.avatar.big_preview.url, alt: '...')
   end
 
   def article_avatar_small_preview(article)
-    return image_tag(asset_path("no-image.png"), alt: "...") if article.avatar_absent_or_disabled?
+    return image_tag('no-image.png', alt: '...') if article.avatar_absent_or_disabled?
 
-    image_tag(article.avatar.small_preview.url, alt: "...")
+    image_tag(article.avatar.small_preview.url, alt: '...')
   end
 
   def utc(time)
-    time.strftime("%d %b %Y, %H:%M")
+    time.strftime('%d %b %Y, %H:%M')
   end
 end
