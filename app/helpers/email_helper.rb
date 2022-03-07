c6ad909bdabf171464506b8d8ae976af04776b28
Artem Popkov <artem.popkov@itechart-group.com>
2022-03-08 00:46:47 +0300

    f

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,6 +1,11 @@
 module EmailHelper
+  # def email_logo_tag(image, **options)
+  #   attachments[image] = File.read(Rails.root.join("app/assets/images/#{image}"))
+  #   image_tag attachments[image].url, **options
+  # end
+
   def email_logo_tag(image, **options)
-    attachments[image] = File.read(Rails.root.join("app/assets/images/#{image}"))
+    attachments[image] = File.read(Rails.root.join("public/#{image}"))
     image_tag attachments[image].url, **options
   end
 
