e194a7e50d0f8a1659c76d2beb1a92dd9a8fdc47
Artem Popkov <artem.popkov@itechart-group.com>
2022-03-07 18:36:19 +0300

    fix account registration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['APPLICATION_HOST'] || '127.0.0.1'
+    origins '*'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
