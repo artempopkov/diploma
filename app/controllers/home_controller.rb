314b5ff8142303b2dbb5ac7852eecb99ed5547a4
Artem Popkov <artem.popkov@itechart-group.com>
2022-03-12 20:46:44 +0300

    fix sort order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @query = Article.published.ransack(params[:query])
     @articles = @query.result.page(current_page).order(published_at: :desc)
-    @three_important_articles = Article.important.published.order(:published_at).last(4)
+    @three_important_articles = Article.important.published.order(published_at: :desc).last(4)
     @tranding_articles = Article.trending.limit(3)
   end
 
