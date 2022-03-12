450dbdff0ef57e969d345294a344cd90292a4375
Artem Popkov <artem.popkov@itechart-group.com>
2022-03-12 23:58:26 +0300

    push

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                            # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],            # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],            # required
-    region:                'eu-central-1'                       # to match the carrierwave and bucket region
+    region:                ENV["AWS_REGION"]                      # to match the carrierwave and bucket region
   }
   config.fog_directory = ENV["AWS_BUCKET"]                   # required
   config.fog_public    = false
