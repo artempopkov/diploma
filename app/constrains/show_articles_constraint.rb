6b7b5420d2b9f710c2810a80f5c7aec362974ee6
Artem Popkov <artem.popkov@itechart-group.com>
2022-03-13 20:14:58 +0300

    fix

--- app/constrains/show_articles_constraint.rb
+++ app/constrains/show_articles_constraint.rb
@@ -1,6 +1,7 @@
 class ShowArticlesConstraint
   def matches?(request)
-    article = Article.find(request.params['id'])
+    articles_id = request.params['id'] || request.params['article_id']
+    article = Article.find(articles_id)
     article.published?
   end
 end
